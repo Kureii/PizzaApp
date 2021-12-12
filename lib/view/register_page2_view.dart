1b93fbfd4f4117524a91c3f2fa7bf6d55894ebd8
Tomáš Adámek <ad.tomik@seznam.cz>
2021-12-12 15:54:57 +0100

    finish register page 2

--- lib/view/register_page2_view.dart
+++ lib/view/register_page2_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pizzaapp/style.dart';
 import 'package:pizzaapp/viewmodel/register_viewmodel.dart';
+import 'package:pizzaapp/view/register_page1_view.dart';
 
 class RegisterPage2View extends StatefulWidget {
   const RegisterPage2View({Key? key}) : super(key: key);
@@ -13,25 +14,22 @@ class _RegisterPage2ViewState extends State<RegisterPage2View> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   var myStyle = Style();
   var regVM = RegisterViewModel();
-  /*final contllorerName = TextEditingController();
-  final contllorerSurname = TextEditingController();
-  final contllorerMail = TextEditingController();
+  final contllorerPswd = TextEditingController();
+  final contllorerVerPswd = TextEditingController();
 
   @override
   void initState() {
-    contllorerName.text = regVM.name;
-    contllorerSurname.text = regVM.surname;
-    contllorerMail.text = regVM.mail;
+    contllorerPswd.text = regVM.passwd;
+    contllorerVerPswd.text = regVM.verPasswd;
     super.initState();
   }
 
   @override
   void dispose() {
-    contllorerName.dispose();
-    contllorerSurname.dispose();
-    contllorerMail.dispose();
+    contllorerPswd.dispose();
+    contllorerVerPswd.dispose();
     super.dispose();
-  }*/
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,14 +37,356 @@ class _RegisterPage2ViewState extends State<RegisterPage2View> {
       body: Center(
         child: Form(
           key: _formKey,
-          child: SizedBox(
-            height: 400,
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 750),
+            height: regVM.page2Width,
+            curve: Curves.bounceOut,
             width: 333,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 // title
                 Text('Registrovat se', style: myStyle.h1,),
+
+                // password
+                AnimatedContainer(
+                  height: regVM.pswdContainerHeight,
+                  margin: const EdgeInsets.fromLTRB(0, 7.5, 0, 0),
+                  duration: const Duration(milliseconds: 750),
+                  curve: Curves.bounceOut,
+                  decoration: BoxDecoration(
+                    color: myStyle.white,
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: [
+                      BoxShadow (
+                        color: myStyle.black.withOpacity(.25),
+                        offset: const Offset(2,2),
+                        blurRadius: 1,
+                        spreadRadius: 0,
+                      ),
+                    ],
+                  ),
+                  child: Column (
+                    children:[
+                      TextFormField(
+                        autofocus: false,
+                        cursorWidth: 2,
+                        cursorRadius: const Radius.circular(2),
+                        cursorColor: myStyle.primary,
+                        controller: contllorerPswd,
+                        keyboardType: TextInputType.visiblePassword,
+                        obscureText: !regVM.pswdVis,
+                        onChanged: (_pswd) async {
+                          setState(() {
+                            regVM.passwd = _pswd;
+                          });
+                          await Future.delayed(Duration(milliseconds: 150));
+                          setState(() {
+                            regVM.canContinue2Logic();
+                            if (regVM.conteLowerCase) {
+                              regVM.lowrCaseColor = myStyle.highlight;
+                            } else {
+                              regVM.lowrCaseColor = myStyle.black;
+                            }
+                            if (regVM.conteCapitals) {
+                              regVM.capitalsColor = myStyle.highlight;
+                            } else {
+                              regVM.capitalsColor = myStyle.black;
+                            }
+                            if (regVM.conteDigits) {
+                              regVM.digitsColor = myStyle.highlight;
+                            } else {
+                              regVM.digitsColor = myStyle.black;
+                            }
+                            if (regVM.conte8chars) {
+                              regVM.charsColor = myStyle.highlight;
+                            } else {
+                              regVM.charsColor = myStyle.black;
+                            }
+                          });
+
+                        },
+                        onTap: () async{
+                          setState(()  {
+                            regVM.page2Width = 432;
+                            regVM.pswdContainerHeight = 138.5;
+                            regVM.pswdProgressBarHeight = 8;
+                            regVM.visPswdStren = true;
+                          });
+                          await Future.delayed(const Duration(milliseconds: 900));
+                          setState(() {
+                            regVM.visPswdTip = true;
+                            regVM.pswdTipColor = myStyle.black;
+                          });
+                          if (regVM.lowrCaseColor == myStyle.white) {
+                            setState(() {
+                              regVM.lowrCaseColor = myStyle.black;
+                              regVM.capitalsColor = myStyle.black;
+                              regVM.digitsColor = myStyle.black;
+                              regVM.charsColor = myStyle.black;
+                            });
+                          }
+                        },
+                        decoration: InputDecoration(
+                          labelText: "Heslo",
+                          labelStyle: myStyle.label,
+                          suffixIcon: Container(
+                            color: myStyle.transparent,
+                            width: 10,
+                            height: 45,
+                            padding: const EdgeInsets.fromLTRB(0, 1, 1, 1),
+                            child: TextButton(
+                              style: TextButton.styleFrom(
+                                backgroundColor: myStyle.transparent,
+                                primary: myStyle.primary,
+                                shape: const RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.horizontal(
+                                    right: Radius.circular(6),
+                                  ),
+                                ),
+                              ),
+                              focusNode: FocusNode(
+                                canRequestFocus: false,
+                                skipTraversal: true,
+                              ),
+                              onLongPress: () {setState(() {regVM.pswdVis = !regVM.pswdVis;});},
+                              onPressed: () {setState(() {regVM.pswdVis = !regVM.pswdVis;});},
+                              child: regVM.pswdVis ? myStyle.hidePswd : myStyle.showPswd,
+                            ),
+                          ),
+                          contentPadding: const EdgeInsets.all(10),
+                          enabledBorder: myStyle.unfocused,
+                          focusedBorder: myStyle.focused,
+                          errorBorder: myStyle.errUnfocused,
+                          focusedErrorBorder: myStyle.errfocused,
+                        ),
+                      ),
+                      Visibility(
+                        visible: regVM.visPswdTip,
+                        child: AnimatedContainer(
+                          height: regVM.pswdContainerHeight - 48,
+                          duration: const Duration(seconds: 1),
+                          curve: Curves.easeInOut,
+                          padding: const EdgeInsets.all(5),
+                          child:Column(
+                            children: [
+                              Align(
+                                alignment: Alignment.center,
+                                child: AnimatedDefaultTextStyle(
+                                  child: const Text(
+                                    "Heslo musí obsahovat:",
+                                    textAlign: TextAlign.center,),
+                                  style: TextStyle(fontSize: 14, color: regVM.pswdTipColor),
+                                  duration: const Duration(seconds: 1),
+                                  curve: Curves.easeInOut,
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: AnimatedDefaultTextStyle(
+                                  child: const Text(
+                                    "• Malé písmena",
+                                    textAlign: TextAlign.left,),
+                                  style: TextStyle(fontSize: 14, color: regVM.lowrCaseColor),
+                                  duration: const Duration(milliseconds: 250),
+                                  curve: Curves.easeInOut,
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: AnimatedDefaultTextStyle(
+                                  child: const Text(
+                                    "• Velké písmena",
+                                    textAlign: TextAlign.left,),
+                                  style: TextStyle(fontSize: 14, color: regVM.capitalsColor),
+                                  duration: const Duration(milliseconds: 250),
+                                  curve: Curves.easeInOut,
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: AnimatedDefaultTextStyle(
+                                  child: const Text(
+                                    "• Číslice",
+                                    textAlign: TextAlign.left,),
+                                  style: TextStyle(fontSize: 14, color: regVM.digitsColor),
+                                  duration: const Duration(milliseconds: 250),
+                                  curve: Curves.easeInOut,
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: AnimatedDefaultTextStyle(
+                                  child: const Text(
+                                    "• 8 znaků",
+                                    textAlign: TextAlign.left,),
+                                  style: TextStyle(fontSize: 14, color: regVM.charsColor),
+                                  duration: const Duration(milliseconds: 250),
+                                  curve: Curves.easeInOut,
+                                ),
+                              ),
+
+                            ]
+                          ),
+                        ),
+                      ),
+                    ]
+                  )
+                ),
+
+                Visibility(
+                  visible: regVM.visPswdStren ,
+                  child: AnimatedContainer(
+                      height: regVM.pswdProgressBarHeight,
+                      width: 333,
+                      clipBehavior: Clip.antiAlias,
+                      duration: const Duration(milliseconds: 1500),
+                      curve: Curves.easeInOutSine,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
+                        color: myStyle.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: myStyle.black.withOpacity(.25),
+                            offset: const Offset(2,2),
+                            blurRadius: 1,
+                            spreadRadius: 0,
+                          ),
+                        ],
+                      ),
+                      child: Align (
+                        alignment: Alignment.centerLeft,
+                        child: AnimatedContainer(
+                          duration: const Duration(milliseconds: 500),
+                          curve:  Curves.bounceOut,
+                          height: 8,
+                          width: regVM.pswdStre(),
+                          decoration: BoxDecoration(
+                              color: regVM.colorPswdStre()
+                          ),
+                        ),
+                      ),
+                    ),
+                ),
+
+                // password verify
+                Container(
+                  decoration: BoxDecoration(
+                    color: myStyle.white,
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: [
+                      BoxShadow (
+                        color: myStyle.black.withOpacity(.25),
+                        offset: const Offset(2,2),
+                        blurRadius: 1,
+                        spreadRadius: 0,
+                      ),
+                    ],
+                  ),
+                  margin: const EdgeInsets.fromLTRB(0, 7.5, 0, 7.5),
+                  child: TextFormField(
+                    autofocus: false,
+                    cursorWidth: 2,
+                    cursorRadius: const Radius.circular(2),
+                    cursorColor: myStyle.primary,
+                    controller: contllorerVerPswd,
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: !regVM.verPswdVis,
+                    autovalidateMode: AutovalidateMode.always,
+                    onChanged: (_pswd) async{
+                      setState(()  {
+                        regVM.verPasswd = _pswd;
+                      });
+                      await Future.delayed(Duration(milliseconds: 150));
+                      setState(() {
+                        regVM.canContinue2Logic();
+                      });
+                    },
+                    validator: (_verPswd) {
+                      if (regVM.verPasswd.length < regVM.passwd.length ||
+                          regVM.verPasswd.length == regVM.passwd.length &&
+                          regVM.verPasswd == regVM.passwd) {
+                        return null;
+                      } else if (regVM.verPasswd.length == regVM.passwd.length &&
+                                  regVM.verPasswd != regVM.passwd) {
+                        return "hesla se neshodují";
+                      } else {
+                        return "hesla se neshodují a jsou jinak dlouhá";
+                      }
+                    },
+                    decoration: InputDecoration(
+                      labelText: "Ověření hesla",
+                      labelStyle: myStyle.label,
+                      suffixIcon: Container(
+                        color: myStyle.transparent,
+                        width: 10,
+                        height: 45,
+                        padding: const EdgeInsets.fromLTRB(0, 1, 1, 1),
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                            backgroundColor: myStyle.transparent,
+                            primary: myStyle.primary,
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.horizontal(
+                                right: Radius.circular(6),
+                              ),
+                            ),
+                          ),
+                          focusNode: FocusNode(
+                            canRequestFocus: false,
+                            skipTraversal: true,
+                          ),
+                          onLongPress: () {setState(() {regVM.verPswdVis = !regVM.verPswdVis;});},
+                          onPressed: () {setState(() {regVM.verPswdVis = !regVM.verPswdVis;});},
+                          child: regVM.verPswdVis ? myStyle.hidePswd : myStyle.showPswd,
+                        ),
+                      ),
+                      contentPadding: const EdgeInsets.all(10),
+                      enabledBorder: myStyle.unfocused,
+                      focusedBorder: myStyle.focused,
+                      errorBorder: myStyle.errUnfocused,
+                      focusedErrorBorder: myStyle.errfocused,
+                    ),
+                  ),
+
+                ),
+
+                //extra space
+                Container(height: 10, color: myStyle.transparent,),
+
+                // continue button
+                Container(
+                  height: 50,
+                  width: 333,
+                  margin: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                  child: ElevatedButton(
+                      style: myStyle.btnStyle(25),
+                      onPressed: regVM.canContinue2 ? () {
+                        //Navigator.push(
+                          //context,
+                          //MaterialPageRoute(builder: (context) => const RegisterPage3View()),
+                        //);
+                      } : null,
+                      child: Text("Pokračovat", style: myStyle.btnText, )
+                  ),
+                ),
+
+                // back button
+                Container(
+                  height: 50,
+                  width: 333,
+                  margin: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                  child: ElevatedButton(
+                      style: myStyle.invBtnStyle(25),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const RegisterPage1View()),
+                        );
+                      },
+                      child: Text("Zpět", style: myStyle.invBtnText, )
+                  ),
+                ),
               ],
             ),
           ),
--- lib/viewmodel/register_viewmodel.dart
+++ lib/viewmodel/register_viewmodel.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:pizzaapp/style.dart';
 
@@ -9,16 +10,38 @@ class RegisterViewModel{
   }
   RegisterViewModel._internal();
 
+  static var myStyle = Style();
 
-  var myStyle = Style();
   bool canContinue1 = false;
+  bool canContinue2 = false;
+  bool canRegister = false;
+  bool pswdVis = false;
+  bool verPswdVis = false;
+  bool visPswdTip = false;
+  bool visPswdStren = false;
+  bool conteLowerCase = false;
+  bool conteCapitals = false;
+  bool conteDigits = false;
+  bool conte8chars = false;
+  double page2Width = 340;
+  double pswdStrenColTmp = 0.0;
+  double pswdContainerHeight = 48.0;
+  double pswdProgressBarHeight = 0.0;
   String name = "";
   String surname = "";
   String mail = "";
   String passwd = "";
   String verPasswd = "";
   String birthdayText= "";
+  Color pswdTipColor = myStyle.white;
+  Color lowrCaseColor = myStyle.white;
+  Color capitalsColor = myStyle.white;
+  Color digitsColor = myStyle.white;
+  Color charsColor = myStyle.white;
+
   DateTime birthday = DateTime.now();
+
+
 
   Future<void> selectDate(BuildContext context) async {
 
@@ -55,10 +78,98 @@ class RegisterViewModel{
   }
 
   canContinue1Logic(){
-    if (name.length > 1 && surname.length > 1 && mail.length > 5 && birthdayText.length > 0) {
+    if (name.length > 1 && surname.length > 1 && mail.length > 5 && birthdayText.length > 1) {
       canContinue1 = true;
     } else {
       canContinue1 = false;
     }
   }
+
+  canContinue2Logic(){
+    if (passwd.length >= 8 && passwd == verPasswd && pswdStrenColTmp > 26) {
+      canContinue2 = true;
+    } else {
+      canContinue2 = false;
+    }
+  }
+
+  canRegisterLogic(){
+
+  }
+
+  pswdStre () {
+    double tmp = 0.0 + passwd.length;
+
+    RegExp chars = RegExp(r"[a-z]",
+                      caseSensitive: true,
+                      unicode: true);
+    if (chars.hasMatch(passwd)) {
+      tmp += 5;
+      conteLowerCase = true;
+    } else {
+      conteLowerCase = false;
+    }
+    RegExp capitals = RegExp(r"[A-Z]",
+                      caseSensitive: true,
+                      unicode: true);
+    if (capitals.hasMatch(passwd)) {
+      tmp += 7;
+      conteCapitals = true;
+    } else {
+      conteCapitals = false;
+    }
+    RegExp numbers = RegExp(r"\d");
+    if (numbers.hasMatch(passwd)) {
+      tmp += 6;
+      conteDigits = true;
+    } else {
+      conteDigits = false;
+    }
+    RegExp specials = RegExp(r"[!@#\$%\^\&*\)\(+=._-]");
+    if (specials.hasMatch(passwd)) tmp += 10;
+    if (passwd.length >= 8) {
+      conte8chars = true;
+    } else {
+      conte8chars = false;
+    }
+    if (passwd.length < 8 &&  tmp > 26) {
+      pswdStrenColTmp = 26;
+      return .26 * 333;
+    } else if (passwd.length >= 8 &&
+        (!conteLowerCase ||
+        !conteCapitals ||
+        !conteDigits)
+    ) {
+      pswdStrenColTmp = 26;
+      return .26 * 333;
+    }
+    else if (passwd.length >= 8 && passwd.length < 16)
+    { tmp += log(passwd.length) + passwd.length * .25; }
+    else if (passwd.length >= 16 && passwd.length < 32)
+    { tmp += pow(1.125, passwd.length); }
+    else if (passwd.length >= 32)
+    { tmp += pow(passwd.length, 1.1); }
+    if (tmp > 100) { tmp = 100; }
+    pswdStrenColTmp = tmp;
+    return tmp * 3.33;
+  }
+
+  colorPswdStre() {
+    int value = pswdStrenColTmp.round();
+    late int red;
+    int blue = 32;
+    late int green;
+    if (value <= 25)
+    {
+      red = 200  - value;
+      green = 2 * value;
+    }
+    else
+    {
+      red = 200 - value + log(value).round();
+      green = 2 * value + log(value).round();
+    }
+    return Color.fromARGB(255, red, green, blue);
+  }
+
 }
